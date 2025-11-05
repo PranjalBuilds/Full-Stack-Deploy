77e7d3e6a2fe7a505e3d7acab580efc8229c4335
PranjalBuilds <imcae22060@gmail.com>
2025-11-05 08:09:59 +0530

    set cross-origins

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("/api/message")
+    fetch("http://localhost:4000/api/message")
     .then((res) => res.json())
     .then((data) => setMessage(data.message))
     .catch((err) => {
--- server/index.js
+++ server/index.js
@@ -5,7 +5,19 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:3000", // serve
+            // will add production URL here
+        ],
+        credentials: true,
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    }
+));
 
 // API Routes
 
